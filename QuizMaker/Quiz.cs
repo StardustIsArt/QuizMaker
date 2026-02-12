fb5c85c147718a2b86d08181541bca9d9d7b5f53
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2026-02-12 09:38:55 -0600

    made Quiz class

--- QuizMaker/Quiz.cs
+++ QuizMaker/Quiz.cs
@@ -3,7 +3,15 @@ namespace QuizMaker;
 public class Quiz
 {
     
-    public string getQuestion {  get; set; }
+    public string getQuestion {  get; set; } 
     public string getCorrectAnswer { get; set; }
     public string getIncorrectAnswer1 { get; set; }
+
+    public Quiz(string question, string correctAnswer, string incorrectAnswer)
+    {
+        getQuestion = question;
+        getCorrectAnswer = correctAnswer;
+        getIncorrectAnswer1 = incorrectAnswer;
+    }
+    
 }
