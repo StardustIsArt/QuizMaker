90b89955d7f08aed7d683e5417b800ef675463b0
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2026-02-14 17:54:09 -0600

    corrected the quiz class and added serialization of the user inputs

--- QuizMaker/Program.cs
+++ QuizMaker/Program.cs
@@ -8,14 +8,23 @@ class Program
     public static void Main(string[] args)
     {
         Messages.DisplayWelcomeMessage();
-        Quiz userQuiz = new Quiz();
-        userQuiz.Question = Messages.Question();
-        userQuiz.Answers = new List<string>();
-        userQuiz.CorrectAnswerIndex = 0;
-        
+        Quiz userQuiz = new Quiz()
+        {
+            Question = Messages.Question(),
+            Answers = new List<string>(),
+            CorrectAnswerIndex = 0
+        };
         userQuiz.Answers.Add(Messages.GetCorrectAnswer());
         userQuiz.Answers.Add(Messages.IncorrectAnswer());
         userQuiz.Answers.Add(Messages.IncorrectAnswer());
     }
-    
+    public static void SaveQuiz()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(Quiz));
+        var xmlFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Quiz.xml");
+        using (FileStream fs = new FileStream(xmlFilePath, FileMode.Create))
+        {
+            serializer.Serialize(fs, new Quiz());
+        }
+    }
 }
